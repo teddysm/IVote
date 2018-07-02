53cb63e58b88d587a839aa543814c9da8be3e68b
teddysm <doan.teddy@gmail.com>
2018-07-01 18:52:24 -0700

    Answers

--- src/iVote/Answers.java
+++ src/iVote/Answers.java
@@ -9,4 +9,20 @@ package iVote;
  */
 public class Answers {
 	private String answer[] = new String[5];
+	private boolean type;
+	public Answers(boolean type) {
+		if (type)
+		{
+			
+		}
+		else
+		{
+			
+		}
+			
+	}
+	
+	public void setSingleAnswer(String answer) {
+		this.answer[0] = answer;
+	}
 }
