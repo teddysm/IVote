b97e52e43209474a506021d68a098e9afef295c2
teddysm <33099760+teddysm@users.noreply.github.com>
2018-07-02 15:45:28 -0700

    fix

--- src/iVote/IVoteService.java
+++ src/iVote/IVoteService.java
@@ -8,10 +8,16 @@ package iVote;
  *
  */
 public class IVoteService {
-	
-	public IVoteService(String[] students, boolean type, String title)
+	private String[] students = new String[10];
+	public IVoteService(boolean type, String title)
 	{
+		for (int i = 0; i < 10; i++)
+		{
+			int random = (int )(Math.random() * 9999 + 1000);
+			students[i] = Integer.toString(random);
+		}
 		Questions question = new Questions(type, title);
+		
 	}
 
 }
--- src/iVote/SimulationDriver.java
+++ src/iVote/SimulationDriver.java
@@ -14,23 +14,13 @@ public class SimulationDriver {
 	 */
 	public static void main(String[] args) {
 		greeting();
-		String[] students = new String[10];
-		students = generatingStudents(students);
 		System.out.println("Simulating a multiple-choice question for CS 356 ");
-		IVoteService test1 = new IVoteService(students, true, "CS 356");
+		IVoteService test1 = new IVoteService(true, "CS 356");
 		System.out.println("\nSimulating a single choice question for CS 356 ");
-		IVoteService test2 = new IVoteService(students, false, "CS 356");
+		IVoteService test2 = new IVoteService(false, "CS 356");
 
 	}
 	
-	private static String[] generatingStudents(String[] students) {
-		for (int i = 0; i < 10; i++)
-		{
-			int random = (int )(Math.random() * 9999 + 1000);
-			students[i] = Integer.toString(random);
-		}
-		return students;
-	}
 
 	public static void greeting() {
 		System.out.println("Welcome to iVote.");
